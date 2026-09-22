773159c164ed4cac32fb3a48b73e9a8de7bc16b6
dipesh-2005 <dipesh.bhattarai.dipu@gmail.com>
2026-09-22 21:10:28 +0545

    rag/pipeline.py

---
+++ rag/pipeline.py
@@ -0,0 +1,18 @@
+from llm.chat import chat
+from retrieval.retriever import retrieve
+
+def ask(question:str):
+    chunks = retrieve(question)
+
+    context = "\n\n".join(
+        chunk["text"]
+        for chunk in chunks
+    )
+
+
+    answer = chat(
+        question=question,
+        context = context
+    )
+
+    return answer
