326ba6864395d61fcdf96cbc1c6c112c6c2b9139
dipesh-2005 <dipesh.bhattarai.dipu@gmail.com>
2026-09-22 21:04:43 +0545

    llm/chat.py

---
+++ llm/chat.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+
+load_dotenv()
+
+llm = ChatGroq(
+    model="openai/gpt-oss-120b",
+    temperature=0
+)
+
+def chat(question: str, context:str):
+    prompt = f"""
+You are a helpful assistant.
+Anawer only using the provided context.
+Context:{context}
+Question:{question}
+"""
+
+    response = llm.invoke(
+    [
+        {
+            "role": "user",
+            "content": prompt
+        }
+    ]
+)
+    return response.content
+
+
