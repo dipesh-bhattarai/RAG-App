81afab8dfbe62f09b0eead0ba1af83a9873f058a
dipesh-2005 <dipesh.bhattarai.dipu@gmail.com>
2026-09-22 20:43:31 +0545

    db/qdrant.py

---
+++ db/qdrant.py
@@ -0,0 +1,41 @@
+from qdrant_client import QdrantClient
+from qdrant_client.models import Distance , PointStruct, VectorParams
+from uuid import uuid4
+
+client = QdrantClient("localhost", port=633)
+
+COLLECTION_NAME = "documents"
+
+def create_collection(vector_size:int):
+    collections = client.get_collections().collections
+    exists = any(
+        collection.name == COLLECTION_NAME
+        for collection in collections
+    )
+
+    if exists:
+        return
+    client.create_collection(
+        collection_name= COLLECTION_NAME,
+        vector_config= VectorParams(
+            size = vector_size,
+            distance = Distance.COSINE
+        )
+    )
+
+
+def store_embeddings(embeddings):
+    points = []
+    for item in embeddings:
+        points.append(
+            PointStruct(
+                id= str(uuid4()),
+                vector = item["embedding"],
+                payload={
+                    "text":item["text"]
+                }
+            )
+        )
+
+
+    client.upsert(collection_name = COLLECTION_NAME, points=points)
