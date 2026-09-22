d1b4a536f46d885584251ad0c34d34be8e36464a
dipesh-2005 <dipesh.bhattarai.dipu@gmail.com>
2026-09-22 21:17:02 +0545

    ingestion/pipeline.py

---
+++ ingestion/pipeline.py
@@ -0,0 +1,20 @@
+from ingestion.chunker import chunk_pdf
+from ingestion.embedder import embed_text
+from db.qdrant import create_collection, store_embeddings
+
+def ingest_pdf(pdf_path):
+    chunks = chunk_pdf(pdf_path)
+
+    embeddings = embed_text(chunks)
+
+    if embeddings:
+        vector_size = len(embeddings[0]["embedding"])
+        create_collection(vector_size)
+
+    store_embeddings(embeddings)
+
+    return {
+        "chunks":len(chunks),
+        "vectors":len(embeddings)
+
+        }
