ee2059711bb2e54bc87dba3e59234b2ad4967262
dipesh-2005 <dipesh.bhattarai.dipu@gmail.com>
2026-09-25 21:22:51 +0545

    Isolate document router

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,26 @@
-from fastapi import FastAPI
-
-from db.qdrant import list_document
+from fastapi import FastAPI, APIRouter
 
 app = FastAPI()
+
+router = APIRouter(
+    prefix="/document",
+    tags=["Documents"]
+)
+
+@router.get("/")
+async def get_documents():
+    return {
+        "message": "document router works"
+    }
+
+@router.delete("/{document_id}")
+async def remove_document(document_id: str):
+    return {
+        "message": "delete works",
+        "document_id": document_id
+    }
+
+app.include_router(router)
 
 
 @app.get("/")
