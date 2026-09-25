2822fc4a56e85aaf15dd66e1db689d644c4ec432
dipesh-2005 <dipesh.bhattarai.dipu@gmail.com>
2026-09-25 21:32:42 +0545

    Test document module import

--- backend/main.py
+++ backend/main.py
@@ -1,26 +1,8 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI
+
+from api.document import router as document_router
 
 app = FastAPI()
-
-router = APIRouter(
-    prefix="/document",
-    tags=["Documents"]
-)
-
-@router.get("/")
-async def get_documents():
-    return {
-        "message": "document router works"
-    }
-
-@router.delete("/{document_id}")
-async def remove_document(document_id: str):
-    return {
-        "message": "delete works",
-        "document_id": document_id
-    }
-
-app.include_router(router)
 
 
 @app.get("/")
